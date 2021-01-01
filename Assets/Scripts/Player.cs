92b448d5102b2dde08d464c0a63ae267020004c9
cammac <camm@rocktechcomputer.com>
2020-12-31 16:27:33 -0800

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float moveSpeed;
+    public Rigidbody2D rig;
+    public float jumpForce;
+
+
+    void FixedUpdate() {
+        float xInput = Input.GetAxis("Horizontal");
+        // float yInput = Input.GetAxis("Vertical");
+
+        rig.velocity = new Vector2(xInput * moveSpeed, rig.velocity.y);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }  
+    }
+}
