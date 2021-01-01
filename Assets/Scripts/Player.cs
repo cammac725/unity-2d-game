1ece3e2223ff00ca9af1e5fd57e0500a1a2d81a7
cammac <camm@rocktechcomputer.com>
2021-01-01 11:52:27 -0800

    modified player jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,8 +18,13 @@ public class Player : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && IsGrounded()) {
             rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }  
+    }
+
+    bool IsGrounded() {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(0, -0.1f, 0), Vector2.down, 0.2f);
+        return hit.collider != null;
     }
 }
